c6c2c4080200e441d2fd75027f4f601402936945
ryotahara123 <157497976+ryotahara123@users.noreply.github.com>
2024-01-29 23:32:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,11 +22,6 @@ def detect_encoding(contents):
     detector.close()
     return detector.result['encoding']
 
-# トップページ
-@app.get('/')
-async def index():
-    return {"Patents": 'Patents_Similarity_App'}
-
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...), query_text: Optional[str] = Form(None)):
     try:
